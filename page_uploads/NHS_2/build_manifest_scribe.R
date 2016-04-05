0c531207c7139f7c98eddd157aad8eb425692ef9
Philip Brohan <philip@brohan.org>
2016-04-05 21:58:24 +0100

    Updated manifest to deal correctly with groups.

--- page_uploads/NHS_2/build_manifest_scribe.R
+++ page_uploads/NHS_2/build_manifest_scribe.R
@@ -6,26 +6,30 @@ library(jpeg)
 panoptes<-readLines('./panoptes_details.csv')
 thumbnails<-readLines('./panoptes_thumbnails.csv')
 
+offset<-0
 for(part in c(1,2,3,4)) {
 
     log.files<-Sys.glob(sprintf("/Users/philip/LocalData/oW4.uploads/NHS_2/for_upload_pt%d/*.jpg",part))
 
     # Redirect ouput to the manifest file
-    sink(sprintf("Albion_pt%d.csv",part))
+    sink(sprintf("group_albion_pt%d.csv",part))
     cat('order,set_key,file_path,thumbnail,width,height,date_created,zooniverse_id\n')
     for(i in seq_along(log.files)) {
 
      im<-readJPEG(log.files[i])
       cat(i,',',sep="")
       cat(sprintf ('Albion %d,',part))
-      cat(substr(panoptes[i],180,288),',',sep="")
-      cat(substr(panoptes[i],180,288),',',sep="")
+      r<-regexpr('http.*jpeg',panoptes[i+offset])
+      cat(regmatches(panoptes[i+offset],r),',',sep="")
+      r<-regexpr('http.*jpeg',thumbnails[i+offset])
+      cat(regmatches(thumbnails[i+offset],r),',',sep="")
       cat(dim(im)[2],',',sep="")
       cat(dim(im)[1],',',sep="")
       cat(format(Sys.time(), "%a %b %d %H:%M:%S %Y",tz="UTC"),',',sep="")
-      cat(substr(panoptes[i],1,7),'\n',sep="")
+      cat(substr(panoptes[i+offset],1,7),'\n',sep="")
     }
     sink() # Output back to default
+    offset<-offset+length(log.files)
 }
   
   
