1644773b82e6988440b37a9651d033208805fe70
Philip Brohan <philip@brohan.org>
2016-11-29 13:48:12 +0000

    Added scripts to add the Alexander images to the site.

---
+++ page_uploads/Batch_3_201611/alexander/build_manifest.panoptes.R
@@ -0,0 +1,22 @@
+# Make the manifest CSV files for the Albion of Fairhaven images
+
+for(part in c(1,2,3,4,5)) {
+
+    log.files<-Sys.glob(sprintf("%s/oW4_logbooks/Batch_3_201611/alexander/for_upload_pt%d/*.jpg",Sys.getenv('SCRATCH'),part))
+
+    # Redirect ouput to the manifest file
+    sink(sprintf("%s/oW4_logbooks/Batch_3_201611/alexander/for_upload_pt%d/manifest.csv",Sys.getenv('SCRATCH'),part))
+    cat('subject_id,image_name_1,origin,ship,port,year\n')
+    for(i in seq_along(log.files)) {
+      cat(i,',',sep="")
+      cat(basename(log.files[i]),',',sep="")
+      cat('New Bedford,')
+      cat('Alexander,')
+      cat('San Francisco,')
+      cat('1903\n')
+    }
+    sink() # Output back to default
+}
+  
+  
+
---
+++ page_uploads/Batch_3_201611/alexander/build_manifest_scribe.R
@@ -0,0 +1,39 @@
+# Make the scribe CSV files for the Alexander images
+
+library(jpeg)
+
+# Get the panoptes subject data
+panoptes<-read.csv('../../old-weather-subjects.csv')
+
+for(part in c(1)) {
+
+    log.files<-Sys.glob(sprintf("%s/oW4_logbooks/Batch_3_201611/alexander/for_upload_pt%d/*.jpg",Sys.getenv('SCRATCH'),part))
+
+    # Redirect ouput to the manifest file
+    sink(sprintf("group_alexander_pt%d.csv",part))
+    cat('order,set_key,file_path,thumbnail,width,height,date_created,zooniverse_id\n')
+    for(i in seq_along(log.files)) {
+
+     im<-readJPEG(log.files[i])
+     w<-grep(basename(log.files[i]),panoptes$metadata)
+     if(length(w)!=2) stop ("Can't find images in panoptes")
+     r<-regexpr('https://panoptes-uploads.*.jpeg',
+                   panoptes$locations[w[1]])
+     p.main<-regmatches(panoptes$locations[w[1]],r)
+     r<-regexpr('https://panoptes-uploads.*.jpeg',
+                panoptes$locations[w[2]])
+     p.thumb<-regmatches(panoptes$locations[w[2]],r)
+      cat(i,',',sep="")
+      cat(sprintf ('Alexander %d,',part))
+      cat(p.main,',',sep="")
+      cat(p.thumb,',',sep="")
+      cat(dim(im)[2],',',sep="")
+      cat(dim(im)[1],',',sep="")
+      cat(format(Sys.time(), "%a %b %d %H:%M:%S %Y",tz="UTC"),',',sep="")
+      cat(panoptes$subject_id[w[1]],'\n',sep="")
+    }
+    sink() # Output back to default
+}
+  
+  
+
